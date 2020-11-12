e45e7b1d0cf5362405e087b93908bc04c71ec97c
coderj001 <amirajubolchi001@gmail.com>
2020-11-12 19:43:25 +0530

    django admin words views

--- note_project/core/admin.py
+++ note_project/core/admin.py
@@ -7,9 +7,11 @@ from core.models import Note
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'updated_at', 'word_count')
+    list_filter = ('updated_at', 'created_at')
+    search_fields = ('title__startswith',)
 
     def word_count(self, obj):
         from django.utils.html import format_html
         result = len((obj.content).split())
-        return format_html("<b>{}</b>", result)
+        return format_html("<b style='color: blue'>{}</b>", result)
     word_count.short_description = "Words"
