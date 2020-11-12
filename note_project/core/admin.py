9a4b6035c1fda331d8d5d699ab107be54bcd021c
coderj001 <amirajubolchi001@gmail.com>
2020-11-12 19:34:48 +0530

    django models and admin setup

--- note_project/core/admin.py
+++ note_project/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import Note
 
 # Register your models here.
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'word_count')
+
+    def word_count(self, obj):
+        from django.utils.html import format_html
+        result = len((obj.content).split())
+        return format_html("<b>{}</b>", result)
+    word_count.short_description = "Words"
--- note_project/core/models.py
+++ note_project/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at', 'updated_at')
+        verbose_name_plural = "EvaNote"
+
+    def __str__(self):
+        return self.title
