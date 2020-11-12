2910c99ecde9d3fb64f3971fb81ca3ab0771fdd3
coderj001 <amirajubolchi001@gmail.com>
2020-11-13 03:25:24 +0530

    frontend react scss setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <h1>Header</h1>
+      <Button variant="primary">Bootstrap Button</Button>
     </div>
   );
 }
